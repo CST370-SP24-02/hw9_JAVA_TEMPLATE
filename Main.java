f04ff28d83728c897dbfd9acce76b9ce3a852326
rampurgeCSUMB <98712477+rampurgeCSUMB@users.noreply.github.com>
2024-04-19 12:06:40 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 /*
  * INSTRUCTION:
- *     This is a Java starting code for hw8.
+ *     This is a Java starting code for hw9.
  *     When you finish the development, commit and push the file to github.
  */
 
 // Finish the head comment with Abstract, ID, Name, and Date.
 /*
- * Title: Main_hw7_*.java
+ * Title: Main_hw9_*.java
  * Abstract: Write the main purpose of the program.
  * Name: Write your name
  * Date: MM/DD/YYYY
